6a6579ef0e2b144f9a85960cddec358cbaeef04a
jagophile <afoster@cfa.harvard.edu>
2015-10-29 18:46:37 +0100

    Added apec.py to __init__.py on import

--- pyatomdb/pyatomdb/__init__.py
+++ pyatomdb/pyatomdb/__init__.py
@@ -1,5 +1,6 @@
-__all__=["atomdb","util","atomic","spectrum","const"]
+__all__=["atomdb","util","atomic","spectrum","const","apec"]
 
 from .atomdb import *
 import spectrum
 import atomic
+import apec
