c792460aa540c86862d6abb634d9212e63227519
jagophile <afoster@cfa.harvard.edu>
2016-11-10 19:53:49 -0500

    Changes for Readthedocs use

--- pyatomdb/pyatomdb/__init__.py
+++ pyatomdb/pyatomdb/__init__.py
@@ -5,10 +5,19 @@ import spectrum
 import atomic
 import ctypes
 import apec
+import sys
 __version__="0.0.2.0"
-PATH = os.path.dirname(__file__)
-liblinapprox_file = os.path.join(PATH,"../linear_approx.dylib")
-if not os.path.isfile(liblinapprox_file):
-  liblinapprox_file = os.path.join(PATH,"../linear_approx.so")
- 
-liblinapprox = ctypes.CDLL( liblinapprox_file, ctypes.RTLD_GLOBAL)
+
+try:
+  PATH = os.path.dirname(__file__)
+  liblinapprox_file = os.path.join(PATH,"../linear_approx.dylib")
+  if not os.path.isfile(liblinapprox_file):
+    liblinapprox_file = os.path.join(PATH,"../linear_approx.so")
+   
+  liblinapprox = ctypes.CDLL( liblinapprox_file, ctypes.RTLD_GLOBAL)
+except OSError:
+  on_rtd=os.environ.get('READTHEDOCS')=='True'
+  if on_rtd:
+    pass
+  else:
+    raise
