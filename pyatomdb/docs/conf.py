8d3b32eebdb423ec00a3db5c0bf6e70577492226
jagophile <afoster@cfa.harvard.edu>
2023-11-20 14:24:19 -0500

    Doc

--- pyatomdb/docs/conf.py
+++ pyatomdb/docs/conf.py
@@ -65,6 +65,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
