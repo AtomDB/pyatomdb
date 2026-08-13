847fe9136c298e6b6701364349212536c547111c
Adam Foster <afoster@cfa.harvard.edu>
2026-08-13 17:42:30 -0400

    Updated solve_ionbal_eigen to work with numpy >=2.5 (issues with matrix data indexing)

--- pyatomdb/pyatomdb/apec.py
+++ pyatomdb/pyatomdb/apec.py
@@ -6071,10 +6071,10 @@ def _solve_ionbal_eigen(Z, Te, init_pop=False, tau=False, \
     for itau, ttau in enumerate(tau_vec):
       if Z >1:
         for i in range(Z):
-          worktmp[i] = numpy.squeeze(fspectmp[i]*numpy.exp(dd.data['EIG'][kTindex,i]*delt*ttau))
+          worktmp[i] = numpy.squeeze(fspectmp.item(i)*numpy.exp(dd.data['EIG'][kTindex,i]*delt*ttau))
 
       else:
-        worktmp[0] = numpy.squeeze(fspectmp[0]*numpy.exp(dd.data['EIG'][kTindex]*delt*ttau))
+        worktmp[0] = numpy.squeeze(fspectmp.item(i)*numpy.exp(dd.data['EIG'][kTindex]*delt*ttau))
 
       frac = numpy.zeros(Z+1)
       for i in range(Z):
